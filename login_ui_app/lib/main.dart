838c11b39f1e7fe28b1d7ccef5d0a6d9bfbc625b
Vedat Biner <vbiner@gmail.com>
2022-12-27 16:36:26 +0300

    Modern UI ready

---
+++ login_ui_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:login_ui_app/pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Sağ üstteki banner kapalı
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
+
+
