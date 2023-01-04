21d1c954e558d9ae064660d63b71be96cc1b1607
Vedat Biner <vbiner@gmail.com>
2023-01-04 16:30:16 +0300

    theme

---
+++ todolist_app/README.md
@@ -0,0 +1,5 @@
+# todolist_app
+
+## Uygulama Teması
+Önce Uygulmamızın teması oluşturuluyor.\
+![ScreenShot](screen_shots/img-01.png)
---
+++ todolist_app/lib/screens/home_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      appBar: AppBar(
+        title: const Text(
+          "Get it Done",
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "5 Items",
+                style: Theme.of(context).textTheme.headline3,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 400,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      30,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
+
