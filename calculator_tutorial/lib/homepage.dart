15dd1e1dbc6b45df222a4c04b9cf9ed3ff2d0ec9
Vedat Biner <vbiner@gmail.com>
2023-01-20 17:32:14 +0300

    03-Evaluate

--- calculator_tutorial/lib/homepage.dart
+++ calculator_tutorial/lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:calculator_tutorial/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,19 +8,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   final myTextStyle = TextStyle(fontSize: 30, color: Colors.deepPurple[900]);
   var userQuestion = "";
   var userAnswer = "";
 
-  final List<String> buttons =
-      [
-        "C", "DEL", "%", "/",
-        "9", "8", "7", "x",
-        "6", "5", "4", "-",
-        "3", "2", "1", "+",
-        "0", ".", "ANS", "=",
-      ];
+  final List<String> buttons = [
+    "C",
+    "DEL",
+    "%",
+    "/",
+    "9",
+    "8",
+    "7",
+    "x",
+    "6",
+    "5",
+    "4",
+    "-",
+    "3",
+    "2",
+    "1",
+    "+",
+    "0",
+    ".",
+    "ANS",
+    "=",
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -38,14 +52,20 @@ class _HomePageState extends State<HomePage> {
                       alignment: Alignment.centerLeft,
                       child: Text(
                         userQuestion,
-                        style: const TextStyle(fontSize: 20,),),
+                        style: const TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
                     ),
                     Container(
                       padding: const EdgeInsets.all(20),
                       alignment: Alignment.centerRight,
                       child: Text(
                         userQuestion,
-                        style: const TextStyle(fontSize: 20,),),
+                        style: const TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
                     )
                   ],
                 ),
@@ -55,47 +75,68 @@ class _HomePageState extends State<HomePage> {
               flex: 2,
               child: Container(
                 child: GridView.builder(
-                  // her sırada dört düğme olsun
-                  itemCount: buttons.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-                  itemBuilder: (BuildContext context, int index){
-                    // Clear button
-                    if (index == 0){
-                      return MyButton(
-                        buttonTapped: (){
-                          setState(() {
-                            userQuestion = "";
-                          });
-                        },
-                        buttonText: buttons[index],
-                        color: Colors.green,
-                        textColor: Colors.white,
-                      );
-                    } else if (index == 1){
-                      // Delete button
+                    // her sırada dört düğme olsun
+                    itemCount: buttons.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4),
+                    itemBuilder: (BuildContext context, int index) {
+                      // Clear button
+                      if (index == 0) {
                         return MyButton(
-                          buttonTapped: (){
+                          buttonTapped: () {
                             setState(() {
-                              userQuestion = userQuestion.substring(0, userQuestion.length - 1);
+                              userQuestion = "";
+                            });
+                          },
+                          buttonText: buttons[index],
+                          color: Colors.green,
+                          textColor: Colors.white,
+                        );
+                      }
+                      // Delete button
+                      else if (index == 1) {
+                        return MyButton(
+                          buttonTapped: () {
+                            setState(() {
+                              userQuestion = userQuestion.substring(
+                                  0, userQuestion.length - 1);
                             });
                           },
                           buttonText: buttons[index],
                           color: Colors.red,
                           textColor: Colors.white,
-                      );
-                    } else {
+                        );
+                      }
+                      // = button
+                      else if (index == buttons.length - 1) {
                         return MyButton(
-                          buttonTapped: (){
+                          buttonTapped: () {
+                            setState(() {
+                              equalPressed();
+                            });
+                          },
+                          buttonText: buttons[index],
+                          color: Colors.deepPurple,
+                          textColor: Colors.white,
+                        );
+                      } else {
+                        return MyButton(
+                          buttonTapped: () {
                             setState(() {
                               userQuestion += buttons[index];
                             });
-                        },
-                        buttonText: buttons[index],
-                        color: isOperator(buttons[index]) ? Colors.deepPurple : Colors.deepPurple.shade50,
-                        textColor: isOperator(buttons[index]) ? Colors.white : Colors.deepPurple,
-                      );
-                  }}
-                ),
+                          },
+                          buttonText: buttons[index],
+                          color: isOperator(buttons[index])
+                              ? Colors.deepPurple
+                              : Colors.deepPurple.shade50,
+                          textColor: isOperator(buttons[index])
+                              ? Colors.white
+                              : Colors.deepPurple,
+                        );
+                      }
+                    }),
               ),
             ),
           ],
@@ -104,22 +145,21 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  bool isOperator(String x){
-    if (x == "%" || x == "/" || x == "x" || x == "-" || x == "+" || x == "="){
+  bool isOperator(String x) {
+    if (x == "%" || x == "/" || x == "x" || x == "-" || x == "+" || x == "=") {
       return true;
     }
     return false;
   }
 
+  void equalPressed() {
+    String finalQuestion = userQuestion;
+    finalQuestion = finalQuestion.replaceAll("x", "*");
+    Parser p = Parser();
+    Expression exp = p.parse(finalQuestion);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+    userAnswer = eval.toString();
+    print(userAnswer);
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
