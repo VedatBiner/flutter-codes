0174a49ca35ebb043d6a306ef8507818d4f6a647
Vedat Biner <vbiner@gmail.com>
2023-01-20 16:43:17 +0300

    02-Inputs

--- calculator_tutorial/lib/buttons.dart
+++ calculator_tutorial/lib/buttons.dart
@@ -5,32 +5,48 @@ class MyButton extends StatelessWidget {
   final Color color;
   final Color textColor;
   final String buttonText;
+  final buttonTapped;
 
   const MyButton({
     super.key,
     required this.color,
     required this.textColor,
-    required this.buttonText
+    required this.buttonText,
+    required this.buttonTapped
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-          child: Container(
-            color: color,
-            child: Center(
-              child: Text(
-                buttonText,
-                style: TextStyle(
-                  color: textColor,
+    return GestureDetector(
+      onTap: buttonTapped,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+            child: Container(
+              color: color,
+              child: Center(
+                child: Text(
+                  buttonText,
+                  style: TextStyle(
+                    color: textColor,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold
+                  ),
                 ),
               ),
             ),
-          ),
+        ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
--- calculator_tutorial/lib/homepage.dart
+++ calculator_tutorial/lib/homepage.dart
@@ -8,6 +8,10 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  final myTextStyle = TextStyle(fontSize: 30, color: Colors.deepPurple[900]);
+  var userQuestion = "";
+  var userAnswer = "";
+
   final List<String> buttons =
       [
         "C", "DEL", "%", "/",
@@ -19,45 +23,83 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepPurple[100],
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              child: GridView.builder(
-                // her sırada dört düğme olsun
-                itemCount: buttons.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-                itemBuilder: (BuildContext context, int index){
-                  if (index == 0){
-                    return MyButton(
-                      buttonText: buttons[index],
-                      color: Colors.green,
-                      textColor: Colors.white,
-                    );
-                  } else if (index == 1){
-                    return MyButton(
-                      buttonText: buttons[index],
-                      color: Colors.red,
-                      textColor: Colors.white,
-                    );
-                  } else {
-                    return MyButton(
-                      buttonText: buttons[index],
-                      color: isOperator(buttons[index]) ? Colors.deepPurple : Colors.deepPurple.shade50,
-                      textColor: isOperator(buttons[index]) ? Colors.white : Colors.deepPurple,
-                    );
-                }}
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.deepPurple[100],
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        userQuestion,
+                        style: const TextStyle(fontSize: 20,),),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        userQuestion,
+                        style: const TextStyle(fontSize: 20,),),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 2,
+              child: Container(
+                child: GridView.builder(
+                  // her sırada dört düğme olsun
+                  itemCount: buttons.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+                  itemBuilder: (BuildContext context, int index){
+                    // Clear button
+                    if (index == 0){
+                      return MyButton(
+                        buttonTapped: (){
+                          setState(() {
+                            userQuestion = "";
+                          });
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.green,
+                        textColor: Colors.white,
+                      );
+                    } else if (index == 1){
+                      // Delete button
+                        return MyButton(
+                          buttonTapped: (){
+                            setState(() {
+                              userQuestion = userQuestion.substring(0, userQuestion.length - 1);
+                            });
+                          },
+                          buttonText: buttons[index],
+                          color: Colors.red,
+                          textColor: Colors.white,
+                      );
+                    } else {
+                        return MyButton(
+                          buttonTapped: (){
+                            setState(() {
+                              userQuestion += buttons[index];
+                            });
+                        },
+                        buttonText: buttons[index],
+                        color: isOperator(buttons[index]) ? Colors.deepPurple : Colors.deepPurple.shade50,
+                        textColor: isOperator(buttons[index]) ? Colors.white : Colors.deepPurple,
+                      );
+                  }}
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -70,6 +112,14 @@ class _HomePageState extends State<HomePage> {
   }
 
 }
+
+
+
+
+
+
+
+
 
 
 
