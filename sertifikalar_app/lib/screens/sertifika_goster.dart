3588bc689e12c083404c3deb9212d490d1eb0359
Vedat Biner <vbiner@gmail.com>
2023-05-04 15:16:27 +0300

    appBar template

--- sertifikalar_app/lib/screens/sertifika_goster.dart
+++ sertifikalar_app/lib/screens/sertifika_goster.dart
@@ -1,4 +1,6 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SertifikaGoster extends StatefulWidget {
   final String resim;
@@ -10,20 +12,42 @@ class SertifikaGoster extends StatefulWidget {
 }
 
 class _SertifikaGosterState extends State<SertifikaGoster> {
+  late Future<Uint8List> _resimYukleme;
+
+  @override
+  void initState() {
+    super.initState();
+    _resimYukleme = _resimYukle();
+  }
+
+  Future<Uint8List> _resimYukle() async {
+    // final adres = "assets/images/${widget.resim}";
+    // final adres = "${widget.resim}";
+    final adres = widget.resim;
+    final ByteData veri = await rootBundle.load(adres);
+    return veri.buffer.asUint8List();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final adres = "assets/images/${widget.resim}";
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sertifika Göster'),
       ),
-      body: Center(
-        child: Image.asset(
-         // "assets/images/${widget.sertResim}",
-         // "assets/images/btk-blokzincirkriptoparalar.jpg",
-          adres,
-        ), // Load the image
+      body: FutureBuilder<Uint8List>(
+        future: _resimYukleme,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Hata: ${snapshot.error}'));
+          }
+          if (snapshot.hasData) {
+            return Center(child: Image.memory(snapshot.data!));
+          }
+          return Container();
+        },
       ),
     );
   }
