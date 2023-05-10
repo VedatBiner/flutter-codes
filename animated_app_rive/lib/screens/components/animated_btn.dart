8b44a08785e2b891c2e6475dbc9f6bb5db2e15ea
Vedat Biner <vbiner@gmail.com>
2023-05-10 08:56:32 +0300

    animated_app_rive

---
+++ animated_app_rive/lib/screens/components/animated_btn.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:rive/rive.dart';
+
+class AnimatedBtn extends StatelessWidget {
+  const AnimatedBtn({
+    super.key,
+    required RiveAnimationController btnAnimationController,
+    required this.press,
+  }) : _btnAnimationController = btnAnimationController;
+
+  final RiveAnimationController _btnAnimationController;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        press;
+      },
+      child: SizedBox(
+        height: 64,
+        width: 260,
+        child: Stack(
+          children: [
+            RiveAnimation.asset(
+              "assets/RiveAssets/button.riv",
+              controllers: [_btnAnimationController],
+            ),
+            Positioned.fill(
+              top: 8,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(CupertinoIcons.arrow_right),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "Start the course",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
