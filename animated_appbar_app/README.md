629d276b88aeed478ce28e9b7e8c29cd8e436f04
Vedat Biner <vbiner@gmail.com>
2023-05-12 13:12:04 +0300

    animated_appbar_app

---
+++ animated_appbar_app/README.md
@@ -0,0 +1,11 @@
+# animated_appbar_app
+
+https://www.youtube.com/playlist?list=PLxUBb2A_UUy8gqrZrob7xGF9ys9PN-XkY sayfasındaki listedeki uygulamadır.
+Uygulama da bazı sorunlar var ama menü tasarımı ve animasyon bir örnek olarak bulunsun istedim.
+<BR>
+Kullanılan Paketler
+•	Rive
+•	Flutter_svg
+<HR>
+<img src="https://github.com/VedatBiner/flutter-codes/blob/master/animated_fab/screen_shots/img-01.png" height="400em"/>
+<img src="https://github.com/VedatBiner/flutter-codes/blob/master/animated_fab/screen_shots/img-02.png" height="400em"/>
---
+++ animated_appbar_app/lib/screens/onboarding/components/animated_btn.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:rive/rive.dart';
+
+class AnimatedBtn extends StatelessWidget {
+  const AnimatedBtn({
+    Key? key,
+    required RiveAnimationController btnAnimationColtroller,
+    required this.press,
+  })  : _btnAnimationColtroller = btnAnimationColtroller,
+        super(key: key);
+
+  final RiveAnimationController _btnAnimationColtroller;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: SizedBox(
+        height: 64,
+        width: 260,
+        child: Stack(
+          children: [
+            // Just a button no animation
+            // Let's fix that
+            RiveAnimation.asset(
+              "assets/RiveAssets/button.riv",
+              // Once we restart the app it shows the animation
+              controllers: [_btnAnimationColtroller],
+            ),
+            const Positioned.fill(
+              // But it's not center
+              top: 8,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(CupertinoIcons.arrow_right),
+                  SizedBox(width: 8),
+                  Text(
+                    "Start the course",
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
