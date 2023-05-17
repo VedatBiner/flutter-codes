024912f49000ebce43d1e183dd63f59fc41110e6
Vedat Biner <vbiner@gmail.com>
2023-05-17 13:07:18 +0300

    Firebase blog App

---
+++ userblog_fb/lib/anasayfa.dart
@@ -0,0 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class AnaSayfa extends StatelessWidget {
+  const AnaSayfa({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Anasayfa")),
+      body: const UserInformation(),
+    );
+  }
+}
+
+class UserInformation extends StatefulWidget {
+  const UserInformation({super.key});
+
+  @override
+  _UserInformationState createState() => _UserInformationState();
+}
+
+class _UserInformationState extends State<UserInformation> {
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('Yazilar').snapshots();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _usersStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading");
+        }
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data =
+                document.data()! as Map<String, dynamic>;
+            return ListTile(
+              title: Text(data['baslik']),
+              subtitle: Text(data['icerik']),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
--- userblog_fb/lib/profil_sayfasi.dart
+++ userblog_fb/lib/profil_sayfasi.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:userblog_fb/yazi_sayfasi.dart';
+import 'anasayfa.dart';
 import 'main.dart';
 
 class ProfilEkrani extends StatelessWidget {
@@ -13,14 +14,23 @@ class ProfilEkrani extends StatelessWidget {
         title: const Text("Profil Sayfası"),
         actions: [
           IconButton(
+            onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const AnaSayfa(),
+                  ),
+                  (Route<dynamic> route) => true);
+            },
+            icon: const Icon(Icons.home),
+          ),
+          IconButton(
             icon: const Icon(Icons.exit_to_app),
             onPressed: () {
               FirebaseAuth.instance.signOut().then((deger) {
                 Navigator.pushAndRemoveUntil(
                     context,
-                    MaterialPageRoute(
-                      builder: (_) => const Iskele(),
-                    ),
+                    MaterialPageRoute(builder: (_) => const Iskele()),
                     (Route<dynamic> route) => false);
               });
             },
@@ -29,13 +39,11 @@ class ProfilEkrani extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: (){
+        onPressed: () {
           Navigator.pushAndRemoveUntil(
               context,
-              MaterialPageRoute(
-                builder: (_) => const YaziEkrani(),
-              ),
-                  (Route<dynamic> route) => true);
+              MaterialPageRoute(builder: (_) => const YaziEkrani()),
+              (Route<dynamic> route) => true);
         },
       ),
       body: Container(),
