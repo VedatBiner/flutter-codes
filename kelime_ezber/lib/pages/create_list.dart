0cf2e5684a3c33729007ac20e5ce3390f5733301
Vedat Biner <vbiner@gmail.com>
2023-05-26 09:18:22 +0300

    kelime_ezber

--- kelime_ezber/lib/pages/create_list.dart
+++ kelime_ezber/lib/pages/create_list.dart
@@ -9,6 +9,30 @@ class CreateList extends StatefulWidget {
 
 class _CreateListState extends State<CreateList> {
   final _listName = TextEditingController();
+  List<TextEditingController> wordTextEditingList = [];
+  List<Row> wordListField = [];
+
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < 10; i++) {
+      wordTextEditingList.add(TextEditingController());
+    }
+    for (int i = 0; i < 5; i++) {
+      wordListField.add(Row(
+        children: [
+          Expanded(
+            child: textFieldBuilder(
+                textEditingController: wordTextEditingList[2 * i]),
+          ),
+          Expanded(
+            child: textFieldBuilder(
+                textEditingController: wordTextEditingList[2 * i + 1]),
+          ),
+        ],
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +48,9 @@ class _CreateListState extends State<CreateList> {
               alignment: Alignment.centerLeft,
               width: MediaQuery.of(context).size.width * 0.20,
               child: InkWell(
-                onTap: () {},
+                onTap: () {
+                  Navigator.pop(context);
+                },
                 child: const Icon(
                   Icons.arrow_back_ios,
                   color: Colors.black,
@@ -53,41 +79,88 @@ class _CreateListState extends State<CreateList> {
           color: Colors.white,
           child: Column(
             children: [
+              textFieldBuilder(
+                icon: const Icon(
+                  Icons.list,
+                  size: 18,
+                ),
+                hintText: "Liste Adı",
+                textEditingController: _listName,
+                textAlign: TextAlign.left,
+              ),
               Container(
-                height: 40,
-                padding: const EdgeInsets.only(left: 16, right: 16),
-                decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.25),
-                  borderRadius: BorderRadius.circular(4),
+                margin: const EdgeInsets.only(top: 20, bottom: 10),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      "İngilizce",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: "RobotoRegular",
+                      ),
+                    ),
+                    Text(
+                      "Türkçe",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: "RobotoRegular",
+                      ),
+                    ),
+                  ],
                 ),
-                margin: const EdgeInsets.only(
-                  left: 16,
-                  right: 16,
-                  top: 4,
-                  bottom: 4,
-                ),
-                child: TextField(
-                  keyboardType: TextInputType.name,
-                  maxLines: 1,
-                  textAlign: TextAlign.left,
-                  controller: _listName,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontFamily: "RobotoMedium",
-                    decoration: TextDecoration.none,
-                    fontSize: 18,
-                  ),
-                  decoration: const InputDecoration(
-                    icon: Icon(Icons.list),
-                    border: InputBorder.none,
-                    hintText: "Liste Adı",
-                    fillColor: Colors.transparent,
-                    isDense: true,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: wordListField,
                   ),
                 ),
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Container textFieldBuilder({
+    int height = 40,
+    required TextEditingController textEditingController,
+    Icon? icon,
+    String? hintText,
+    textAlign = TextAlign.center,
+  }) {
+    return Container(
+      height: double.parse(height.toString()),
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      decoration: BoxDecoration(
+        color: Colors.grey.withOpacity(0.25),
+        borderRadius: BorderRadius.circular(4),
+      ),
+      margin: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 4,
+        bottom: 4,
+      ),
+      child: TextField(
+        keyboardType: TextInputType.name,
+        maxLines: 1,
+        textAlign: textAlign,
+        controller: textEditingController,
+        style: const TextStyle(
+          color: Colors.black,
+          fontFamily: "RobotoMedium",
+          decoration: TextDecoration.none,
+          fontSize: 18,
+        ),
+        decoration: InputDecoration(
+          icon: icon,
+          border: InputBorder.none,
+          hintText: "Liste Adı",
+          fillColor: Colors.transparent,
+          isDense: true,
         ),
       ),
     );
