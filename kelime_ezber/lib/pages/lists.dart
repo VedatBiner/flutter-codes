26350e9000f5bec4cc1154dfb909a408bfedba70
Vedat Biner <vbiner@gmail.com>
2023-05-30 17:38:46 +0300

    kelime_ezber

--- kelime_ezber/lib/pages/lists.dart
+++ kelime_ezber/lib/pages/lists.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:kelime_ezber/database/db/dbhelper.dart';
 import 'package:kelime_ezber/widgets/appbar_page.dart';
-import '../widgets/list_item.dart';
+import 'package:kelime_ezber/widgets/toat_message.dart';
+import '../methods.dart';
 import 'create_list.dart';
 
 class ListsPage extends StatefulWidget {
@@ -13,6 +14,8 @@ class ListsPage extends StatefulWidget {
 
 class _ListsPageState extends State<ListsPage> {
   List<Map<String, Object?>> _lists = [];
+  bool pressController = false;
+  List<bool> deleteIndexList = [];
 
   @override
   void initState() {
@@ -22,9 +25,37 @@ class _ListsPageState extends State<ListsPage> {
 
   void getList() async {
     _lists = await DbHelper.instance.readListsAll();
+    for (int i = 0; i < _lists.length; i++) {
+      deleteIndexList.add(false);
+    }
     setState(() {
       _lists;
     });
+  }
+
+  void delete() async {
+    List<int> removeIndexList = [];
+    for (int i = 0; i < _lists.length; i++) {
+      if (deleteIndexList[i] == true) {
+        removeIndexList.add(i);
+      }
+    }
+    for (int i = removeIndexList.length - 1; i >= 0; i--) {
+      // veri tabanından silme işlemi
+      DbHelper.instance.deleteListsAndWordByList(
+          _lists[removeIndexList[i]]['list_id'] as int);
+      _lists.removeAt(removeIndexList[i]);
+      deleteIndexList.removeAt(removeIndexList[i]);
+    }
+    for (int i = 0; i > deleteIndexList.length; i++) {
+      deleteIndexList[i] = false;
+    }
+    setState(() {
+      _lists;
+      deleteIndexList;
+      pressController = false;
+    });
+    toastMessage("Seçili listeler silinmiştir.");
   }
 
   @override
@@ -37,11 +68,22 @@ class _ListsPageState extends State<ListsPage> {
             size: 22,
           ),
           center: Image.asset("assets/images/lists.png"),
-          right: Image.asset(
-            "assets/images/logo.png",
-            height: 35,
-            width: 35,
-          ),
+          right: pressController != true
+              ? Image.asset(
+                  "assets/images/logo.png",
+                  height: 35,
+                  width: 35,
+                )
+              : InkWell(
+                  onTap: () {
+                    delete;
+                  },
+                  child: const Icon(
+                    Icons.delete,
+                    color: Colors.deepPurpleAccent,
+                    size: 24,
+                  ),
+                ),
           leftWidgetOnClick: () => {
                 Navigator.pop(context),
               }),
@@ -62,12 +104,126 @@ class _ListsPageState extends State<ListsPage> {
           itemBuilder: (context, index) {
             return listItem(
               _lists[index]["list_id"] as int,
+              index,
               listName: _lists[index]['name'].toString(),
               sumWords: _lists[index]['sum_word'].toString(),
               sumUnlearned: _lists[index]['sum_unlearned'].toString(),
             );
           },
           itemCount: _lists.length,
+        ),
+      ),
+    );
+  }
+
+  InkWell listItem(
+    int id,
+    int index, {
+    required String? listName,
+    required String? sumWords,
+    required String? sumUnlearned,
+  }) {
+    return InkWell(
+      onTap: () {
+        print(id.toString());
+      },
+      onLongPress: () {
+        setState(() {
+          pressController = true;
+          deleteIndexList[index] = true;
+        });
+      },
+      child: SizedBox(
+        width: double.infinity,
+        child: Card(
+          color: Color(
+            RenkMetod.HexaColorConverter("#DCD2FF"),
+          ),
+          elevation: 8,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          margin:
+              const EdgeInsets.only(left: 10, right: 10, top: 5, bottom: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 15, top: 5),
+                    child: Text(
+                      listName!,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontFamily: "RobotoMedium",
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 30),
+                    child: Text(
+                      "${sumWords!} terim",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontFamily: "RobotoRegular",
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 30),
+                    child: Text(
+                      "${int.parse(sumWords) - int.parse(sumUnlearned!)} öğrenildi",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontFamily: "RobotoRegular",
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 30, bottom: 5),
+                    child: Text(
+                      "$sumUnlearned öğrenilmedi",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontFamily: "RobotoRegular",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              pressController == true
+                  ? Checkbox(
+                      checkColor: Colors.white,
+                      activeColor: Colors.deepPurpleAccent,
+                      hoverColor: Colors.blueAccent,
+                      value: deleteIndexList[index],
+                      onChanged: (bool? value) {
+                        setState(() {
+                          deleteIndexList[index] = value!;
+                          bool deleteProcessController = false;
+                          deleteIndexList.forEach((element) {
+                            if (element == true) {
+                              deleteProcessController = true;
+                            }
+                          });
+                          // liste eski haline geri dönüyor
+                          if (!deleteProcessController) {
+                            pressController = false;
+                          }
+                        });
+                      },
+                    )
+                  : Container(),
+            ],
+          ),
         ),
       ),
     );
