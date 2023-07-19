5142a6db655611e4993f40137b65aad61017bd97
Vedat Biner <vbiner@gmail.com>
2023-07-19 18:07:59 +0300

    Portfolio Web App

--- portfolio_web/lib/sections/service/service_section.dart
+++ portfolio_web/lib/sections/service/service_section.dart
@@ -1,10 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_web/constants.dart';
 
 class ServiceSection extends StatelessWidget {
   const ServiceSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: kDefaultPadding * 2),
+      constraints: const BoxConstraints(maxWidth: 1110),
+      child: const Column(
+        children: [
+          SectionTitle(),
+        ],
+      ),
+    );
+  }
+}
+
+class SectionTitle extends StatelessWidget {
+  const SectionTitle({
+    super.key,
+    required this.title,
+    required this.subTitle,
+    required this.color,
+  });
+
+  final String title, subTitle;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+      constraints: const BoxConstraints(maxWidth: 1110),
+      height: 100,
+      child: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+            padding: const EdgeInsets.only(bottom: 72),
+            width: 8,
+            height: 100,
+            color: Colors.black,
+            child: const DecoratedBox(
+              decoration: BoxDecoration(color: Color(0xFFFF0000)),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text(
+                "My Strong Arenas",
+                style: TextStyle(
+                  fontWeight: FontWeight.w200,
+                  color: kTextColor,
+                ),
+              ),
+              Text(
+                "Service Offerings",
+                style: Theme.of(context).textTheme.displayMedium!.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
