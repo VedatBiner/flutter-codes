415e551eb771e8836e0e21e04e53d98159641e10
Vedat Biner <vbiner@gmail.com>
2023-07-20 09:59:31 +0300

    Portfolio Web App

--- portfolio_web/lib/sections/service/service_section.dart
+++ portfolio_web/lib/sections/service/service_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_web/constants.dart';
+import 'components/section_title.dart';
 
 class ServiceSection extends StatelessWidget {
   const ServiceSection({super.key});
@@ -11,64 +12,14 @@ class ServiceSection extends StatelessWidget {
       constraints: const BoxConstraints(maxWidth: 1110),
       child: const Column(
         children: [
-          SectionTitle(),
-        ],
-      ),
-    );
-  }
-}
-
-class SectionTitle extends StatelessWidget {
-  const SectionTitle({
-    super.key,
-    required this.title,
-    required this.subTitle,
-    required this.color,
-  });
-
-  final String title, subTitle;
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: kDefaultPadding),
-      constraints: const BoxConstraints(maxWidth: 1110),
-      height: 100,
-      child: Row(
-        children: [
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: kDefaultPadding),
-            padding: const EdgeInsets.only(bottom: 72),
-            width: 8,
-            height: 100,
-            color: Colors.black,
-            child: const DecoratedBox(
-              decoration: BoxDecoration(color: Color(0xFFFF0000)),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              const Text(
-                "My Strong Arenas",
-                style: TextStyle(
-                  fontWeight: FontWeight.w200,
-                  color: kTextColor,
-                ),
-              ),
-              Text(
-                "Service Offerings",
-                style: Theme.of(context).textTheme.displayMedium!.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-              ),
-            ],
+          SectionTitle(
+            color: Color(0xFFFF0000),
+            title: "Service Offerings",
+            subTitle: "My Strong Arenas",
           ),
         ],
       ),
     );
   }
 }
+
