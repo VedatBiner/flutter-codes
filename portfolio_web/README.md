16f644bd3a7e3e4a85376573bf60d16508300ff5
Vedat Biner <vbiner@gmail.com>
2023-07-21 08:16:20 +0300

    Portfolio Web App

--- portfolio_web/README.md
+++ portfolio_web/README.md
@@ -9,9 +9,11 @@ Portoflio Web Site
 Programın orijinal kodları burada : https://github.com/abuanwar072/Protfolio-Website-Flutter-Web
 
 Flutter güncellemeleri nedeni ile Orijinal kodun üzerinde bazı buton değişiklikleri oldu.
-
+<BR> 
+Uygulamanın ekran görüntüleri
 <HR>
 <img src="https://github.com/VedatBiner/flutter-codes/blob/master/portfolio_web/screen_shots/img-01.png" height="550em"/>
 <img src="https://github.com/VedatBiner/flutter-codes/blob/master/portfolio_web/screen_shots/img-02.png" height="310em"/>
 <img src="https://github.com/VedatBiner/flutter-codes/blob/master/portfolio_web/screen_shots/img-03.png" height="360em"/>
 <img src="https://github.com/VedatBiner/flutter-codes/blob/master/portfolio_web/screen_shots/img-04.png" height="220em"/>
+<img src="https://github.com/VedatBiner/flutter-codes/blob/master/portfolio_web/screen_shots/img-05.png" height="220em"/>
--- portfolio_web/lib/sections/recent_work/recent_work_section.dart
+++ portfolio_web/lib/sections/recent_work/recent_work_section.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_web/components/section_title.dart';
 import 'package:portfolio_web/constants.dart';
+import 'package:portfolio_web/models/recentwork.dart';
 import '../../components/hireme_card.dart';
+import 'components/recent_work_card.dart';
 
 class RecentWorkSection extends StatelessWidget {
   const RecentWorkSection({super.key});
@@ -11,7 +13,6 @@ class RecentWorkSection extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.only(top: kDefaultPadding * 6),
       width: double.infinity,
-      height: 600,
       decoration: BoxDecoration(
         color: const Color(0xFFF7E8FF).withOpacity(0.3),
         image: const DecorationImage(
@@ -32,6 +33,23 @@ class RecentWorkSection extends StatelessWidget {
             subTitle: "My Strong Arenas",
             color: Color(0xFFFFB100),
           ),
+          const SizedBox(
+            height: kDefaultPadding * 1.5,
+          ),
+          SizedBox(
+            width: 1110,
+            child: Wrap(
+              spacing: kDefaultPadding,
+              runSpacing: kDefaultPadding * 2,
+              children: List.generate(
+                  recentWorks.length,
+                  (index) => RecentWorkCard(
+                        index: index,
+                        press: () {},
+                      )),
+            ),
+          ),
+          const SizedBox(height: kDefaultPadding * 5),
         ],
       ),
     );
