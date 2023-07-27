3f394ebf9296109111e5e7c1b5080c07fb925931
Vedat Biner <vbiner@gmail.com>
2023-07-27 15:15:43 +0300

    BTK - Anket Firebase Firestore App

--- btk_anket/lib/main.dart
+++ btk_anket/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -22,13 +23,94 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Anket"),
+          backgroundColor: Colors.blueAccent,
+          foregroundColor: Colors.white,
         ),
-        body: SurveyList(),
+        body: const SurveyList(),
       ),
     );
   }
 }
 
+final sahteSnapshot = [
+  {"isim": "C#", "oy": 3},
+  {"isim": "Java", "oy": 4},
+  {"isim": "Dart", "oy": 5},
+  {"isim": "C++", "oy": 7},
+  {"isim": "Python", "oy": 90},
+  {"isim": "Perl", "oy": 2},
+];
+
+class Anket {
+  String isim;
+  int oy;
+  DocumentReference reference;
+
+  Anket({required this.isim, required this.oy, required this.reference});
+
+  factory Anket.fromMap(
+      Map<dynamic, dynamic> map, DocumentReference reference) {
+    return Anket(
+      isim: map["isim"],
+      oy: map["oy"],
+      reference: reference,
+    );
+  }
+
+  factory Anket.fromSnapshot(DocumentSnapshot snapshot) {
+    return Anket.fromMap(
+        snapshot.data() as Map<String, dynamic>, snapshot.reference);
+  }
+}
+
+class SurveyList extends StatefulWidget {
+  const SurveyList({super.key});
+
+  @override
+  State<SurveyList> createState() => _SurveyListState();
+}
+
+class _SurveyListState extends State<SurveyList> {
+  @override
+  Widget build(BuildContext context) {
+    return buildBody(context, sahteSnapshot);
+  }
+
+  Widget buildBody(BuildContext context, List<Map<dynamic, dynamic>> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20),
+      children: snapshot
+          .map<Widget>(
+            (data) => buildListItem(context, data),
+          )
+          .toList(),
+    );
+  }
+
+  Widget buildListItem(BuildContext context, Map data) {
+    final DocumentReference dummyReference =
+        FirebaseFirestore.instance.collection('dilanketi').doc();
+    final row = Anket.fromMap(data, dummyReference);
+    return Padding(
+      key: ValueKey(row.isim),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(row.isim),
+          trailing: Text(row.oy.toString()),
+          onTap: () => print(row.isim),
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
 class SurveyList extends StatelessWidget {
   const SurveyList({super.key});
 
@@ -62,3 +144,5 @@ class SurveyList extends StatelessWidget {
     );
   }
 }
+
+ */
