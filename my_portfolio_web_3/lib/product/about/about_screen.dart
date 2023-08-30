b3b295924726b2764645f01185d454b3dcf12699
Vedat Biner <vbiner@gmail.com>
2023-08-30 15:18:11 +0300

    My Portfolio Web - 3

--- my_portfolio_web_3/lib/product/about/about_screen.dart
+++ my_portfolio_web_3/lib/product/about/about_screen.dart
@@ -15,16 +15,90 @@ class AboutScreen extends StatelessWidget {
         vertical: GapEnum.xxL.value,
       ),
       child: SingleChildScrollView(
-      child: childrenWidget(),
+        child: childrenWidget(),
       ),
     );
   }
 
   Column childrenWidget() => Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      const AboutTitleWidget(),
-      AboutSubtitleWidget(),
-    ],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const AboutTitleWidget(),
+          AboutSubtitleWidget(),
+          GapEnum.L.heightBox,
+          informations(),
+        ],
+      );
+}
+
+Widget informations() {
+  return SizedBox(
+    height: 400,
+    width: 600,
+    child: test(),
   );
 }
+
+GridView test() {
+  return GridView.builder(
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 2,
+      crossAxisSpacing: 8,
+      mainAxisSpacing: 8,
+      mainAxisExtent: 48,
+    ),
+    itemCount: info.length,
+    itemBuilder: (context, index) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("${info[index].title} : ${info[index].description}"),
+          const Divider(),
+        ],
+      );
+    },
+  );
+}
+
+class InformationModel {
+  InformationModel birthday;
+  InformationModel website;
+  InformationModel degree;
+  InformationModel city;
+  InformationModel age;
+  InformationModel mail;
+  InformationModel phone;
+  InformationModel freelance;
+
+  InformationModel(
+    this.birthday,
+    this.website,
+    this.degree,
+    this.city,
+    this.age,
+    this.mail,
+    this.phone,
+    this.freelance,
+  );
+}
+
+class InformationSubModel {
+  String title;
+  String description;
+
+  InformationSubModel(
+    this.title,
+    this.description,
+  );
+}
+
+List<InformationSubModel> info = [
+  InformationSubModel("Birthday", "01.01.1970"),
+  InformationSubModel("Website", "www.vedatbiner.xyz"),
+  InformationSubModel("Degree", "Flutter School"),
+  InformationSubModel("City", "Ankaraz"),
+  InformationSubModel("Age", "58"),
+  InformationSubModel("Email", "vbiner@gmail.com"),
+  InformationSubModel("Phone", "1234567"),
+  InformationSubModel("Freelance", "available"),
+];
