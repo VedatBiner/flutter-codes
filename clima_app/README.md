88fe549b68dccb34d46b4ea96f08721a7055ba72
Vedat Biner <vbiner@gmail.com>
2023-09-18 10:13:48 +0300

    Clima App

---
+++ clima_app/README.md
@@ -0,0 +1,7 @@
+# clima_app
+
+https://www.linkedin.com/learning/flutter-part-08-powering-your-app-with-live-web-data/clima-a-modern-weather-app adresinde 
+yer alan LinkedIn Learning eğitimi uygulamasıdır.
+
+Uygulama eski olduğu için FlatButton ve RaisedButton artık kullanılmıyor. FlatButton yerine TextButton ve TextButtonTheme, 
+RaisedButton yerine ElevatedButton ve ElevatedButtonTheme kullanılıyor. 
---
+++ clima_app/lib/screens/loading_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  Future<void> getLocation() async {
+    Position position = await Geolocator().getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Get the current location
+          },
+          child: const Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
