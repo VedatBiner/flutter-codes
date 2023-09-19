a000475173be28d3e2d7cba5c4a14c4e2ff6bcb2
Vedat Biner <vbiner@gmail.com>
2023-09-19 09:35:46 +0300

    Clima App

--- clima_app/lib/screens/loading_screen.dart
+++ clima_app/lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import '../services/location.dart';
 
@@ -14,6 +17,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async {
@@ -23,8 +27,28 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print("Longitude : ${location.longitude}");
   }
 
+  void getData() async {
+    /// openweather API call
+    String url =
+        "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=0b2583d6a7f4e97dc266081c4d73a7b8";
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+      double temperature = decodedData["current"]["temp"];
+      int condition = decodedData["current"]["weather"][0]["id"];
+      String cityName = decodedData["name"];
+      print(temperature);
+      print(condition);
+      print(cityName);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return const Scaffold(
       body: Center(),
     );
