01f75b7989356fdfb77ab674cc747cdac1e1e6bd
Vedat Biner <vbiner@gmail.com>
2023-10-13 09:30:35 +0300

    Flash Chat App

---
+++ flash_chat_app/README.md
@@ -0,0 +1,4 @@
+# flash_chat_app
+
+LinkedIn Learning - https://www.linkedin.com/learning/flutter-part-10-firebase-cloud-firestore/flash-chat-a-lightning-fast-messaging-app?resume=false eğitimindeki kodlardır.
+Burada firebase ile bir chat uygulaması yapılacaktır. Başlangıç kodları buradadır : https://github.com/londonappbrewery/flash-chat-flutter 
---
+++ flash_chat_app/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../screens/welcome_screen.dart';
+import '../screens/login_screen.dart';
+import '../screens/registration_screen.dart';
+import '../screens/chat_screen.dart';
+
+void main() => runApp(const FlashChat());
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => const WelcomeScreen(),
+        LoginScreen.id: (context) => const LoginScreen(),
+        RegistrationScreen.id: (context) => const RegistrationScreen(),
+        ChatScreen.id: (context) => const ChatScreen(),
+      },
+    );
+  }
+}
