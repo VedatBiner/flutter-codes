9f4842ec1029da3e944a95e38994cb7ae6e81fef
Vedat Biner <vbiner@gmail.com>
2023-10-25 15:57:44 +0300

    Responsive Web App 2

---
+++ responsive_web_app/lib/main.dart
@@ -0,0 +1,48 @@
+/// ----- main.dart -----
+import 'package:flutter/material.dart';
+import 'package:easy_dynamic_theme/easy_dynamic_theme.dart';
+
+import '../utils/authentication.dart';
+import '../utils/theme_data.dart';
+import '../screens/home_page.dart';
+
+void main() {
+  runApp(
+    EasyDynamicThemeWidget(
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future getUserInfo() async {
+    await getUser();
+    setState(() {});
+    print(uid);
+  }
+
+  @override
+  void initState() {
+    getUserInfo();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Explore',
+      theme: lightThemeData,
+      darkTheme: darkThemeData,
+      debugShowCheckedModeBanner: false,
+      themeMode: EasyDynamicTheme.of(context).themeMode,
+      home: HomePage(),
+    );
+  }
+}
