fe08898a0d241a0721b1a077b6e4371b8e86fbfa
Vedat Biner <vbiner@gmail.com>
2023-11-21 13:14:28 +0300

    Tutorial App

--- tutorial_app/lib/views/home/appbar/home_appbar.dart
+++ tutorial_app/lib/views/home/appbar/home_appbar.dart
@@ -1,25 +1,55 @@
 /// <----- home_appbar.dart ----->
-library;
 
-import 'package:flutter/material.dart';
-import 'package:tutorial_app/core/extension/context_extension.dart';
+part of "../home_view.dart";
 
-import '../../../constant/app_const.dart';
-
-class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const HomeAppBar({super.key});
+class _HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const _HomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeNotifier = AppConst.listener.themeNotifier;
     return AppBar(
       title: Text(
         "${context.language ? AppConst.home.themeText.tr : AppConst.home.themeText.en}",
       ),
+
       /// Bir önceki ekrana dönüşü engellemek için adım 2.
       automaticallyImplyLeading: false,
+      actions: [
+        IconButton(
+          onPressed: () {
+            AppConst.listener.language.changeLang();
+          },
+          icon: const Icon(Icons.language),
+        ),
+        const SizedBox(width: 16),
+        IconButton(
+          onPressed: () => AppConst.listener.themeNotifier.changeTheme(),
+          icon: Icon(
+            themeNotifier.isDarkMode ? Icons.wb_sunny : Icons.brightness_3,
+          ),
+        ),
+        const SizedBox(width: 8),
+        IconButton(
+          onPressed: () => Navigator.pushNamed(context, AppRoute.splash),
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+        ),
+      ],
     );
   }
 
   @override
   Size get preferredSize => const Size.fromHeight(100);
 }
+
+
+
+
+
+
+
+
+
+
--- tutorial_app/lib/views/home/home_view.dart
+++ tutorial_app/lib/views/home/home_view.dart
@@ -9,48 +9,19 @@ import '../../core/extension/context_extension.dart';
 
 /// parts field
 part "body/home_body.dart";
-// part "appbar/home_appbar.dart";
+part "appbar/home_appbar.dart";
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final themeNotifier = AppConst.listener.themeNotifier;
 
     /// Bir önceki ekrana dönüşü engellemek için adım 1.
-    return PopScope(
+    return const PopScope(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "${context.language ? AppConst.home.themeText.tr : AppConst.home.themeText.en}",
-          ),
-          /// Bir önceki ekrana dönüşü engellemek için adım 2.
-          automaticallyImplyLeading: false,
-          actions: [
-            IconButton(
-              onPressed: () {
-                AppConst.listener.language.changeLang();
-              },
-              icon: const Icon(Icons.language),
-            ),
-            const SizedBox(width: 16),
-            IconButton(
-              onPressed: () => AppConst.listener.themeNotifier.changeTheme(),
-              icon: Icon(
-                themeNotifier.isDarkMode ? Icons.wb_sunny : Icons.brightness_3,
-              ),
-            ),
-            const SizedBox(width: 8),
-            IconButton(
-              onPressed: () => Navigator.pushNamed(context, AppRoute.splash),
-              icon: const Icon(
-                Icons.arrow_back,
-              ),
-            ),
-          ],
-        ),
-        body: const _HomeBody(),
+        body: _HomeBody(),
+        appBar: _HomeAppBar(),
       ),
     );
   }
