0791bbb6f39a2228e1f4dd5a5a2d363c83577d40
Vedat Biner <vbiner@gmail.com>
2023-11-26 17:22:35 +0300

    TensorFlow Lite Object Detection App

--- tf_ai_object_detector/lib/views/camera_view.dart
+++ tf_ai_object_detector/lib/views/camera_view.dart
@@ -15,7 +15,28 @@ class CameraView extends StatelessWidget {
         init: ScanController(),
         builder: (controller) {
           return controller.isCameraInitialized.value
-              ? CameraPreview(controller.cameraController)
+              ? Stack(
+                  children: [
+                    CameraPreview(controller.cameraController),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(
+                          color: Colors.green,
+                          width: 4.0,
+                        ),
+                      ),
+                      child: const Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text("label of object"),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
               : const Center(
                   child: Text("Loading preview .."),
                 );
