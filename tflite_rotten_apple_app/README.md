d814f95a1c779abd60d40b9e1a02a6347dc84506
Vedat Biner <vbiner@gmail.com>
2023-11-30 09:07:14 +0300

    TensorFlow Lite Rotten Apple App

---
+++ tflite_rotten_apple_app/README.md
@@ -0,0 +1,3 @@
+# tflite_rotten_apple_app
+
+https://www.youtube.com/watch?v=fBqObfoSjVg&ab_channel=CodePie adresindeki uygulama.
---
+++ tflite_rotten_apple_app/lib/model.dart
@@ -0,0 +1,111 @@
+/// <----- model.dart ----->
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
+
+class Model extends StatefulWidget {
+  const Model({super.key});
+
+  @override
+  State<Model> createState() => _ModelState();
+}
+
+class _ModelState extends State<Model> {
+  late File _image;
+  bool selImage = false;
+  List result = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    Tflite.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Rotten Apple Detection"),
+          backgroundColor: Colors.indigo,
+        ),
+        body: Column(
+          children: [
+            Expanded(child: (selImage) ? Image.file(_image) : Container()),
+            const SizedBox(height: 30),
+            (result.isEmpty)
+                ? Container()
+                : Text(
+                    result[0]["labels"],
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+            InkWell(
+              onTap: () {
+                chooseFile();
+              },
+              child: Container(
+                margin: const EdgeInsets.only(top: 20),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.indigoAccent,
+                ),
+                child: const Text(
+                  "Pick an Image",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> chooseFile() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (image!.path != null) {
+      setState(() {
+        selImage = true;
+        _image = File(image.path);
+      });
+    }
+    predictImage(_image);
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/model_unquant.tflite",
+      labels: "assets/labels.txt",
+    );
+  }
+
+  predictImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 5,
+      threshold: 0.5,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      result = output!;
+    });
+    print("result is : $result");
+  }
+}
