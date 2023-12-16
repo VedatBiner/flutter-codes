ee207532aa95e86bc9b97e3c63874d844e1a3643
Vedat Biner <vbiner@gmail.com>
2023-12-16 14:27:29 +0300

    Firebase Firestore SER-TR Dictionary App

--- sozluk_app_ser_tr_fbfs/lib/screens/home_page.dart
+++ sozluk_app_ser_tr_fbfs/lib/screens/home_page.dart
@@ -1,5 +1,4 @@
 /// <----- home_page.dart ----->
-import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -212,67 +211,15 @@ class _HomePageState extends State<HomePage> {
                               ),
                               IconButton(
                                 onPressed: () {
-                                  log("delete word ?");
                                   showDialog(
                                     context: context,
                                     builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(16.0),
-                                          side: const BorderSide(
-                                            color: Colors.red,
-                                            width: 2.0,
-                                          ),
-                                        ),
-                                        title: const Text(
-                                          "Dikkat !!!",
-                                          style: TextStyle(
-                                            color: Colors.red,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                        content: Row(
-                                          children: [
-                                            const Text("Bu kelime "),
-                                            Text(
-                                              "(${word.sirpca})",
-                                              style: const TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.blueAccent,
-                                                fontSize: 16,
-                                              ),
-                                            ),
-                                            const Text(" silinsin mi ?"),
-                                          ],
-                                        ),
-                                        actions: [
-                                          TextButton(
-                                            child: const Text("İptal"),
-                                            onPressed: () {
-                                              log("İptal seçildi");
-                                              Navigator.pop(context);
-                                            },
-                                          ),
-                                          TextButton(
-                                            child: const Text(
-                                              "Tamam",
-                                              style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                            onPressed: () {
-                                              log("Tamam seçildi");
-                                              firestoreService.deleteWord(word.wordId);
-                                              Navigator.pop(context);
-                                            },
-                                          ),
-                                        ],
+                                      return deleteWord(
+                                        word: word,
+                                        firestoreService: firestoreService,
                                       );
                                     },
                                   );
-
-                                  /// firestoreService.deleteWord(word.wordId);
                                 },
                                 icon: const Icon(Icons.delete),
                                 tooltip: "kelime sil",
@@ -291,6 +238,90 @@ class _HomePageState extends State<HomePage> {
           }
         },
       ),
+    );
+  }
+}
+
+class deleteWord extends StatelessWidget {
+  const deleteWord({
+    super.key,
+    required this.word,
+    required this.firestoreService,
+  });
+
+  final Words word;
+  final FirestoreService firestoreService;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+        side: const BorderSide(
+          color: Colors.red,
+          width: 2.0,
+        ),
+      ),
+      title: const Text(
+        "Dikkat !!!",
+        style: TextStyle(
+          color: Colors.red,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      content: Row(
+        children: [
+          const Text("Bu kelime "),
+          Text(
+            "(${word.sirpca})",
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blueAccent,
+              fontSize: 16,
+            ),
+          ),
+          const Text(" silinsin mi ?"),
+        ],
+      ),
+      actions: [
+        TextButton(
+          child: const Text("İptal"),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        TextButton(
+          child: const Text(
+            "Tamam",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          onPressed: () {
+            firestoreService.deleteWord(word.wordId);
+            Navigator.pop(context);
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Row(
+                  children: [
+                    Text(
+                      "(${word.sirpca})",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const Text(
+                      " kelimesi silinmiştir ...",
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
