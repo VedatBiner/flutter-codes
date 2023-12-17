e917e39b748b92e325ba1aefd9c96d33d6da42fe
Vedat Biner <vbiner@gmail.com>
2023-12-17 10:16:16 +0300

    Firebase Firestore SER-TR Dic. App - AppBar - flags

--- sozluk_app_ser_tr_fbfs/lib/screens/details_page.dart
+++ sozluk_app_ser_tr_fbfs/lib/screens/details_page.dart
@@ -1,4 +1,7 @@
 /// <----- details_page.dart ----->
+
+import 'package:flag/flag.dart';
+import 'package:flag/flag_widget.dart';
 import 'package:flutter/material.dart';
 
 import '../models/words.dart';
@@ -26,20 +29,50 @@ class _DetailsPageState extends State<DetailsPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text(
-              widget.word.sirpca,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 40,
-                color: Colors.red,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Flag.fromCode(
+                  FlagsCode.ME,
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.fill,
+                  flagSize: FlagSize.size_1x1,
+                  borderRadius: 25,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  widget.word.sirpca,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              widget.word.turkce,
-              style: const TextStyle(
-                fontSize: 40,
-                color: Colors.blue,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Flag.fromCode(
+                  FlagsCode.TR,
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.fill,
+                  flagSize: FlagSize.size_1x1,
+                  borderRadius: 25,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  widget.word.turkce,
+                  style: const TextStyle(
+                    fontSize: 40,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
