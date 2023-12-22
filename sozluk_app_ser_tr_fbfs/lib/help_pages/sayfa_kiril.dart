7c158f29769d36e8cb3dce71be4e6a030a67b1fc
Vedat Biner <vbiner@gmail.com>
2023-12-22 08:49:10 +0300

    Firebase Firestore SER-TR Dic. App - Drawer menu

--- sozluk_app_ser_tr_fbfs/lib/help_pages/sayfa_kiril.dart
+++ sozluk_app_ser_tr_fbfs/lib/help_pages/sayfa_kiril.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class SayfaKiril extends StatefulWidget {
   const SayfaKiril({Key? key}) : super(key: key);
 
@@ -10,14 +12,66 @@ class SayfaKiril extends StatefulWidget {
 class _SayfaKirilState extends State<SayfaKiril> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Kiril Alfabesi",
-        style: TextStyle(
-          color: Colors.black54,
-          fontSize: 36,
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Sırpça 'da Кiril Harfleri",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Sırpça 'da Kiril Harfleri"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(25),
+          child: SingleChildScrollView(
+            child: Table(
+              columnWidths: const {
+                0: FlexColumnWidth(20),
+                1: FlexColumnWidth(50),
+              },
+              children: kirilAlphabet.map((latin) {
+                return TableRow(children: [
+                  Container(
+                    color: kirilAlphabet.indexOf(latin) % 2 == 0
+                        ? Colors.blue[50]
+                        : Colors.amber[50],
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      latin['turkce'].toString(),
+                      style: kirilAlphabet.indexOf(latin) == 0
+                          ? const TextStyle(fontWeight: FontWeight.bold, fontSize: 16,)
+                          : const TextStyle(fontWeight: FontWeight.normal, fontSize: 14,),
+                    ),
+                  ),
+                  Container(
+                    color: kirilAlphabet.indexOf(latin) % 2 == 0
+                        ? Colors.blue[50]
+                        : Colors.amber[50],
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      latin['sirpca'],
+                      style: kirilAlphabet.indexOf(latin) == 0
+                          ? const TextStyle(fontWeight: FontWeight.bold, fontSize: 16,)
+                          : const TextStyle(fontWeight: FontWeight.normal, fontSize: 14,),
+                    ),
+                  )
+                ]);
+              }).toList(),
+              border: TableBorder.all(width: 1, color: Colors.black),
+            ),
+          ),
+        ));
   }
 }
