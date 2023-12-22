23e1831a7b3bd4bb45ce57f3d720d3e9b1d57612
Vedat Biner <vbiner@gmail.com>
2023-12-22 09:04:49 +0300

    Firebase Firestore SER-TR Dic. App - Drawer menu

--- sozluk_app_ser_tr_fbfs/lib/help_pages/sayfa_kiril.dart
+++ sozluk_app_ser_tr_fbfs/lib/help_pages/sayfa_kiril.dart
@@ -2,20 +2,16 @@
 import 'package:flutter/material.dart';
 
 import '../constants.dart';
+import '../widgets/alphabet_table.dart';
 
-class SayfaKiril extends StatefulWidget {
+class SayfaKiril extends StatelessWidget {
   const SayfaKiril({Key? key}) : super(key: key);
 
-  @override
-  State<SayfaKiril> createState() => _SayfaKirilState();
-}
-
-class _SayfaKirilState extends State<SayfaKiril> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Sırpça 'da Кiril Harfleri",
+      title: "Sırpça 'da Kiril Harfleri",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -25,54 +21,18 @@ class _SayfaKirilState extends State<SayfaKiril> {
 }
 
 class MyHomePage extends StatelessWidget {
-
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Sırpça 'da Kiril Harfleri"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(25),
-          child: SingleChildScrollView(
-            child: Table(
-              columnWidths: const {
-                0: FlexColumnWidth(20),
-                1: FlexColumnWidth(50),
-              },
-              children: kirilAlphabet.map((latin) {
-                return TableRow(children: [
-                  Container(
-                    color: kirilAlphabet.indexOf(latin) % 2 == 0
-                        ? Colors.blue[50]
-                        : Colors.amber[50],
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      latin['turkce'].toString(),
-                      style: kirilAlphabet.indexOf(latin) == 0
-                          ? const TextStyle(fontWeight: FontWeight.bold, fontSize: 16,)
-                          : const TextStyle(fontWeight: FontWeight.normal, fontSize: 14,),
-                    ),
-                  ),
-                  Container(
-                    color: kirilAlphabet.indexOf(latin) % 2 == 0
-                        ? Colors.blue[50]
-                        : Colors.amber[50],
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      latin['sirpca'],
-                      style: kirilAlphabet.indexOf(latin) == 0
-                          ? const TextStyle(fontWeight: FontWeight.bold, fontSize: 16,)
-                          : const TextStyle(fontWeight: FontWeight.normal, fontSize: 14,),
-                    ),
-                  )
-                ]);
-              }).toList(),
-              border: TableBorder.all(width: 1, color: Colors.black),
-            ),
-          ),
-        ));
+      appBar: AppBar(
+        title: const Text("Sırpça 'da Kiril Harfleri"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: AlphabetTable(alphabet: kirilAlphabet),
+      ),
+    );
   }
 }
--- sozluk_app_ser_tr_fbfs/lib/help_pages/sayfa_latin.dart
+++ sozluk_app_ser_tr_fbfs/lib/help_pages/sayfa_latin.dart
@@ -1,9 +1,8 @@
 /// <----- sayfa_latin.dart ----->
-/// https://www.kindacode.com/article/working-with-table-in-flutter/
-/// table example
 import 'package:flutter/material.dart';
 
 import '../constants.dart';
+import '../widgets/alphabet_table.dart';
 
 class SayfaLatin extends StatelessWidget {
   const SayfaLatin({Key? key}) : super(key: key);
@@ -22,54 +21,18 @@ class SayfaLatin extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Sırpça 'da Latin Harfleri"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(25),
-          child: SingleChildScrollView(
-            child: Table(
-              columnWidths: const {
-                0: FlexColumnWidth(20),
-                1: FlexColumnWidth(50),
-              },
-              children: latinAlphabet.map((latin) {
-                return TableRow(children: [
-                  Container(
-                    color: latinAlphabet.indexOf(latin) % 2 == 0
-                        ? Colors.blue[50]
-                        : Colors.amber[50],
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      latin['turkce'].toString(),
-                      style: latinAlphabet.indexOf(latin) == 0
-                          ? const TextStyle(fontWeight: FontWeight.bold, fontSize: 16,)
-                          : const TextStyle(fontWeight: FontWeight.normal, fontSize: 14,),
-                    ),
-                  ),
-                  Container(
-                    color: latinAlphabet.indexOf(latin) % 2 == 0
-                        ? Colors.blue[50]
-                        : Colors.amber[50],
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      latin['sirpca'],
-                      style: latinAlphabet.indexOf(latin) == 0
-                          ? const TextStyle(fontWeight: FontWeight.bold, fontSize: 16,)
-                          : const TextStyle(fontWeight: FontWeight.normal, fontSize: 14,),
-                    ),
-                  )
-                ]);
-              }).toList(),
-              border: TableBorder.all(width: 1, color: Colors.black),
-            ),
-          ),
-        ));
+      appBar: AppBar(
+        title: const Text("Sırpça 'da Latin Harfleri"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: AlphabetTable(alphabet: latinAlphabet),
+      ),
+    );
   }
 }
