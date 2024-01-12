94cced8d999e1605510c86aff375ec7a56d99712
Vedat Biner <vbiner@gmail.com>
2024-01-12 10:05:50 +0300

    Firebase Firestore SER-TR Dic. App - theme provider

--- sozluk_app_ser_tr_fbfs/lib/provider/theme_provider.dart
+++ sozluk_app_ser_tr_fbfs/lib/provider/theme_provider.dart
@@ -1,5 +1,10 @@
+/// <----- theme_provider.dart ----->
+/// tema değişikliği için provider paketi kullanan metot
+
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+
+import '../constants/app_constants/constants.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeMode themeMode = ThemeMode.system;
@@ -22,17 +27,26 @@ class ThemeProvider extends ChangeNotifier {
 class MyThemes {
   static final darkTheme = ThemeData(
     scaffoldBackgroundColor: Colors.grey.shade900,
-    appBarTheme: const AppBarTheme(foregroundColor: Colors.white),
+    appBarTheme: AppBarTheme(foregroundColor: drawerColor),
     primaryColor: Colors.black,
     colorScheme: const ColorScheme.dark(),
-    iconTheme: IconThemeData(color: Colors.purple.shade200, opacity: 0.8),
+    iconTheme: IconThemeData(color: drawerColor),
   );
 
   static final lightTheme = ThemeData(
     scaffoldBackgroundColor: Colors.white,
-    appBarTheme: const AppBarTheme(foregroundColor: Colors.black),
+    appBarTheme: AppBarTheme(backgroundColor: drawerColor),
     primaryColor: Colors.white,
     colorScheme: const ColorScheme.light(),
     iconTheme: const IconThemeData(color: Colors.red, opacity: 0.8),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(drawerColor),
+      ),
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(color: Colors.black), // Tablo içeriği için metin rengini siyah yaptık
+      bodyMedium: TextStyle(color: Colors.black), // İsterseniz bodyText2'yi de güncelleyebilirsiniz
+    ),
   );
 }
