9c5dadc535ed6d79b27b1ae653987b58ed7e7b9b
Vedat Biner <vbiner@gmail.com>
2024-01-21 10:24:42 +0300

    Firebase Firestore SER-TR Dic. App - table fix

--- sozluk_app_ser_tr_fbfs/lib/help_pages/help_parts/build_table.dart
+++ sozluk_app_ser_tr_fbfs/lib/help_pages/help_parts/build_table.dart
@@ -5,13 +5,13 @@ import 'package:flutter/material.dart';
 import '../../constants/app_constants/constants.dart';
 
 Widget buildTable(
-  BuildContext context,
-  List<Map<String, String>> pageSample,
-  String baslik,
-  List<String Function(Map<String, String>)> getColumnValues,
-) {
+    BuildContext context,
+    List<Map<String, String>> pageSample,
+    String baslik,
+    List<String Function(Map<String, String>)> getColumnValues,
+    ) {
   return Padding(
-    padding: const EdgeInsets.all(20),
+    padding: const EdgeInsets.all(12),
     child: SingleChildScrollView(
       child: Column(
         children: [
@@ -25,36 +25,51 @@ Widget buildTable(
           Table(
             columnWidths: {
               for (var index
-                  in List.generate(getColumnValues.length, (index) => index))
+              in List.generate(getColumnValues.length, (index) => index))
                 index: const IntrinsicColumnWidth(),
             },
-            children: pageSample.map((user) {
-              List<Widget> columns = getColumnValues.map((getColumnValue) {
-                String columnValue = getColumnValue(user) ?? '';
-                return Container(
-                  color: pageSample.indexOf(user) == 0
-                      ? Colors.indigo
-                      : pageSample.indexOf(user) % 2 == 0
-                          ? Colors.blue[50]
-                          : Colors.amber[50],
-                  padding: const EdgeInsets.all(15),
-                  child: Text(
-                    columnValue,
-                    style: pageSample.indexOf(user) == 0
-                        ? const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                            color: Colors.white,
-                          )
-                        : const TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 14,
-                          ),
+            children: [
+              TableRow(
+                decoration: const BoxDecoration(
+                  color: Colors.indigo,
+                ),
+                children: getColumnValues.map((getColumnValue) {
+                  String columnHeader = getColumnValue(pageSample.first) ?? '';
+                  return Container(
+                    padding: const EdgeInsets.all(15),
+                    child: Text(
+                      columnHeader,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+              for (var user in pageSample)
+                TableRow(
+                  decoration: BoxDecoration(
+                    color: pageSample.indexOf(user) % 2 == 0
+                        ? Colors.blue[50]
+                        : Colors.amber[50],
                   ),
-                );
-              }).toList();
-              return TableRow(children: columns);
-            }).toList(),
+                  children: getColumnValues.map((getColumnValue) {
+                    String columnValue = getColumnValue(user) ?? '';
+                    return Container(
+                      padding: const EdgeInsets.all(15),
+                      child: Text(
+                        columnValue,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+            ],
             border: TableBorder.all(
               width: 1,
               color: Colors.black,
