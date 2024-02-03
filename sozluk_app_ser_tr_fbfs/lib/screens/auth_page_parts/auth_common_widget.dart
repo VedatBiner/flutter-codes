2cabd1b26f50dec0720f87cadf0aae5c074e3e25
Vedat Biner <vbiner@gmail.com>
2024-02-03 17:18:58 +0300

    Sozluk (SER-TR) App - google sign in

--- sozluk_app_ser_tr_fbfs/lib/screens/auth_page_parts/auth_common_widget.dart
+++ sozluk_app_ser_tr_fbfs/lib/screens/auth_page_parts/auth_common_widget.dart
@@ -3,22 +3,28 @@
 /// Login ekranında e-mail ve password
 /// kutularını gösteren metod
 ///
+/// Google sign in metodo
+///
 library;
 
 import 'package:flutter/material.dart';
 
 import '../../constants/app_constants/constants.dart';
+import '../../services/app_routes.dart';
+import '../../services/auth_services.dart';
 
 class AuthPageWidgets {
+  /// email ve password kutularını oluştur
+  /// bu bölüm login ve register dosyalarında ortak kullanılıyor.
   static Container buildLoginTextField(
-      String hintText,
-      IconData prefixIcon, {
-        bool obscureText = false,
-        TextInputType? keyboardType,
-        Function(String)? onChanged,
-        bool isFirst = false,
-        TextEditingController? controller,
-      }) {
+    String hintText,
+    IconData prefixIcon, {
+    bool obscureText = false,
+    TextInputType? keyboardType,
+    Function(String)? onChanged,
+    bool isFirst = false,
+    TextEditingController? controller,
+  }) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16),
@@ -63,6 +69,58 @@ class AuthPageWidgets {
         ),
         cursorColor: Colors.white,
         keyboardType: keyboardType,
+      ),
+    );
+  }
+
+  /// Google Sign In Butonu
+  static SizedBox googleSignIn(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: RawMaterialButton(
+        fillColor: Colors.black54,
+        elevation: 10,
+        padding: const EdgeInsets.symmetric(vertical: 15),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+          side: const BorderSide(width: 1),
+        ),
+        onPressed: () async {
+          await MyAuthService().signInWithGoogle().then(
+            (value) async {
+              await Navigator.pushNamedAndRemoveUntil(
+                context,
+                AppRoute.home,
+                (route) => false,
+              );
+            },
+          );
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 30,
+              width: 30,
+              child: Image.asset(
+                "assets/images/google.png",
+                errorBuilder: (context, error, stackTrace) {
+                  print("Hata oluştu: $error");
+                  return const Icon(Icons.error);
+                },
+              ),
+            ),
+            const SizedBox(width: 20),
+            Text(
+              "Google ile Giriş",
+              style: TextStyle(
+                color: menuColor,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
