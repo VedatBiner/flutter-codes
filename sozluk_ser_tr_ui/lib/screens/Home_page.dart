3183feff7ac6e3c46ea69a2aa682d09b1f7e258c
Vedat Biner <vbiner@gmail.com>
2024-04-20 15:36:03 +0300

    Sözlük App Ser-Tr - UI version fixes

--- sozluk_ser_tr_ui/lib/screens/Home_page.dart
+++ sozluk_ser_tr_ui/lib/screens/Home_page.dart
@@ -1,12 +1,20 @@
+/// <----- home_page.dart ----->
+library;
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../constants/app_constants/constants.dart';
 import '../models/fs_words.dart';
+import '../models/words.dart';
+import '../services/firestore_services.dart';
 import '../services/icon_provider.dart';
 import '../services/word_service.dart';
 import '../utils/generate_json.dart';
+import '../utils/mesaj_helper.dart';
+import '../widgets/test_entry.dart';
 import 'home_page_parts/bottom_sheet.dart';
 import 'home_page_parts/drawer_items.dart';
 import 'home_page_parts/fab_helper.dart';
@@ -27,6 +35,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   /// JSON dosya oluşturup güncel tutalım
   late WordService _wordService = WordService();
+  /// Firestore servisi için değişken oluşturalım
+  final FirestoreService firestoreService = FirestoreService();
 
   bool isListView = false;
   bool aramaYapiliyorMu = false;
@@ -39,6 +49,10 @@ class _HomePageState extends State<HomePage> {
   String secondLanguageCode = 'TR'; // İkinci dil kodu
   String secondLanguageText = 'Türkçe'; // İkinci dil metni
   String appBarTitle = appBarMainTitleSecond;
+
+  /// veri girişi için Controller
+  final TextEditingController ikinciDilController = TextEditingController();
+  final TextEditingController birinciDilController = TextEditingController();
 
   Future<void> initializeFirestore() async {
     final collectionRef = FirebaseFirestore.instance
@@ -95,6 +109,7 @@ class _HomePageState extends State<HomePage> {
       builder: (context, AsyncSnapshot<List<QuerySnapshot<FsWords>>> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Transform.scale(
+            alignment: Alignment.center,
             scale: 0.30,
             child: const CircularProgressIndicator(
               backgroundColor: Colors.red,
@@ -149,10 +164,9 @@ class _HomePageState extends State<HomePage> {
         ),
         body: showBody(context),
         drawer: buildDrawer(context),
-        floatingActionButton: buildFloatingActionButton(onPressed: () {}
-
-            /// onPressed: () => openWordBox(context: context),
-            ),
+        floatingActionButton: buildFloatingActionButton(
+          onPressed: () => openWordBox(context: context),
+        ),
         bottomSheet: const BottomSheetWidget(),
       ),
     );
@@ -217,4 +231,156 @@ class _HomePageState extends State<HomePage> {
       },
     );
   }
+
+  Future<void> openWordBox({
+    String? docId,
+    required BuildContext context,
+    Words? word,
+  }) async {
+    String action = "create";
+    String secondLang = "";
+    String firstLang = "";
+    String message = "";
+
+    /// eğer currentUserMail null ise sabit değer alsın
+    String currentUserEmail =
+        FirebaseAuth.instance.currentUser?.email ?? 'vbiner@gmail.com';
+
+    if (docId != null) {
+      action = "update";
+
+      /// Firestore 'dan belge verilerini al
+      var snapshot = await firestoreService.getWordById(docId);
+      if (snapshot.exists) {
+        var data = snapshot.data() as Map<String, dynamic>;
+        secondLang = data[fsIkinciDil];
+        firstLang = data[fsBirinciDil];
+      }
+    }
+
+    ikinciDilController.text = secondLang;
+    birinciDilController.text = firstLang;
+
+    /// showDialog içinde kullanılıyor
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: Theme.of(context).brightness == Brightness.dark
+            ? Colors.grey.shade300 // Dark mode için mavi renk
+            : null, // null olması default rengi kullanır
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+          side: const BorderSide(
+            color: Colors.red,
+            width: 2.0,
+          ),
+        ),
+        content: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            /// Seçilen dil birinci dil ise
+            /// seçilen dil üstte yazılır
+            if (firstLanguageText == birinciDil)
+              TextEntry(
+                controller: birinciDilController,
+                hintText: "$birinciDil kelime giriniz ...",
+              ),
+            if (secondLanguageText == ikinciDil)
+              TextEntry(
+                controller: ikinciDilController,
+                hintText: "$ikinciDil karşılığını giriniz ...",
+              ),
+
+            /// Seçilen dil ikinci dil ise
+            /// seçilen dil üstte yazılır.
+            if (firstLanguageText == ikinciDil)
+              TextEntry(
+                controller: ikinciDilController,
+                hintText: "$ikinciDil kelime giriniz ...",
+              ),
+            if (secondLanguageText == birinciDil)
+              TextEntry(
+                controller: birinciDilController,
+                hintText: "$birinciDil karşılığını giriniz ...",
+              ),
+          ],
+        ),
+        actions: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.indigoAccent,
+            ),
+            onPressed: () async {
+              /// iki dil satırı da kelime boş ise eklenmesin
+              if (docId == null &&
+                  ikinciDilController.text == "" &&
+                  birinciDilController.text == "") {
+                MessageHelper.showSnackBar(
+                  context,
+                  message: "İki kelime satırını da boş ekleyemezsiniz ...",
+                );
+              } else if (docId == null) {
+                /// kelime ekleniyor
+                firestoreService.addWord(
+                  context,
+                  ikinciDilController.text,
+                  birinciDilController.text,
+                );
+                message = addMsg;
+              } else {
+                /// kelime güncelleniyor
+                firestoreService.updateWord(
+                  docId,
+                  ikinciDilController.text,
+                  birinciDilController.text,
+                );
+                message = updateMsg;
+              }
+
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            ikinciDilController.text ?? '',
+                            style: kelimeStil,
+                          ),
+                          const Text(" kelimesi "),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            currentUserEmail,
+                            style: userStil,
+                          ),
+                          Text(
+                            message,
+                          ),
+                        ],
+                      ),
+
+                    ],
+                  ),
+                ),
+              );
+
+              /// Controller içeriklerini temizliyoruz
+              ikinciDilController.clear();
+              birinciDilController.clear();
+
+              Navigator.pop(context);
+            },
+            child: Text(
+              docId == null ? "Kelime ekle" : "Kelime düzelt",
+              style: butonTextDialog,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
 }
