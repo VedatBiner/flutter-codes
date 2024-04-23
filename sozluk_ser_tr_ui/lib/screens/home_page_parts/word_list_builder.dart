63eacddfa2449def2496df65d162ed1888b175dd
Vedat Biner <vbiner@gmail.com>
2024-04-23 15:58:42 +0300

    Sözlük App Ser-Tr - UI version fixes

--- sozluk_ser_tr_ui/lib/screens/home_page_parts/word_list_builder.dart
+++ sozluk_ser_tr_ui/lib/screens/home_page_parts/word_list_builder.dart
@@ -45,136 +45,144 @@ class WordListBuilder extends StatelessWidget {
     required bool isListView,
   }) {
     final themeProvider = Provider.of<ThemeProvider>(context);
-    Widget wordWidget;
-    if (isListView) {
-      /// Liste görünümü
-      wordWidget = ListTile(
-        contentPadding: const EdgeInsets.symmetric(horizontal: 8),
-        title: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    word.sirpca ?? "",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: themeProvider.isDarkMode
-                          ? cardDarkModeText1
-                          : cardLightModeText1,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    word.turkce ?? "",
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                      color: themeProvider.isDarkMode
-                          ? cardDarkModeText2
-                          : cardLightModeText2,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 12,
-                    ),
-                  ),
-                ),
-              ],
+    final bool isDarkMode = themeProvider.isDarkMode;
+
+    final wordWidget = isListView
+
+        /// Liste görünümü
+        ? buildListView(word, isDarkMode)
+        : buildCardView(isDarkMode, word, context);
+
+    return wordWidget;
+  }
+
+  Padding buildCardView(bool isDarkMode, FsWords word, BuildContext context) {
+    return Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: Card(
+            elevation: 6,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
             ),
-            Divider(
-              color: themeProvider.isDarkMode ? Colors.white60 : Colors.black45,
-            ),
-          ],
-        ),
-      );
-    } else {
-      /// Card Görünümü
-      wordWidget = Padding(
-        padding: const EdgeInsets.all(2.0),
-        child: Card(
-          elevation: 6,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16.0),
-          ),
-          shadowColor: Colors.green[200],
-          color: themeProvider.isDarkMode ? cardDarkMode : cardLightMode,
-          child: InkWell(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+            shadowColor: Colors.green[200],
+            color: isDarkMode ? cardDarkMode : cardLightMode,
+            child: InkWell(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            word.sirpca ?? "",
+                            style: TextStyle(
+                              color: isDarkMode
+                                  ? cardDarkModeText1
+                                  : cardLightModeText1,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                            ),
+                          ),
+                          Divider(
+                            thickness: 1,
+                            color:
+                                isDarkMode ? Colors.white60 : Colors.black45,
+                          ),
+                          Text(
+                            word.turkce ?? "",
+                            style: TextStyle(
+                              color: isDarkMode
+                                  ? cardDarkModeText2
+                                  : cardLightModeText2,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
                       children: [
-                        Text(
-                          word.sirpca ?? "",
-                          style: TextStyle(
-                            color: themeProvider.isDarkMode
-                                ? cardDarkModeText1
-                                : cardLightModeText1,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                          ),
+                        IconButton(
+                          onPressed: () {
+                            log("Kelime düzeltme seçildi");
+                          },
+                          icon: const Icon(Icons.edit),
+                          tooltip: "kelime düzelt",
+                          color: Colors.green,
                         ),
-                        Divider(
-                          thickness: 1,
-                          color: themeProvider.isDarkMode
-                              ? Colors.white60
-                              : Colors.black45,
-                        ),
-                        Text(
-                          word.turkce ?? "",
-                          style: TextStyle(
-                            color: themeProvider.isDarkMode
-                                ? cardDarkModeText2
-                                : cardLightModeText2,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                          ),
+                        IconButton(
+                          onPressed: () {
+                            log("Kelime silme seçildi");
+                          },
+                          icon: const Icon(Icons.delete),
+                          tooltip: "kelime sil",
+                          color: Colors.red,
                         ),
                       ],
                     ),
+                  ],
+                ),
+              ),
+              onTap: () {
+                log("word : ${word.sirpca}");
+                log("word : ${word.turkce}");
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const DetailsPage(),
                   ),
-                  Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          log("Kelime düzeltme seçildi");
-                        },
-                        icon: const Icon(Icons.edit),
-                        tooltip: "kelime düzelt",
-                        color: Colors.green,
+                );
+              },
+            ),
+          ),
+        );
+  }
+
+  ListTile buildListView(FsWords word, bool isDarkMode) {
+    return ListTile(
+          contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+          title: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      word.sirpca ?? "",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: isDarkMode
+                            ? cardDarkModeText1
+                            : cardLightModeText1,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
                       ),
-                      IconButton(
-                        onPressed: () {
-                          log("Kelime silme seçildi");
-                        },
-                        icon: const Icon(Icons.delete),
-                        tooltip: "kelime sil",
-                        color: Colors.red,
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      word.turkce ?? "",
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        color: isDarkMode
+                            ? cardDarkModeText2
+                            : cardLightModeText2,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
                       ),
-                    ],
+                    ),
                   ),
                 ],
               ),
-            ),
-            onTap: () {
-              log("word : ${word.sirpca}");
-              log("word : ${word.turkce}");
-
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const DetailsPage(),
-                ),
-              );
-            },
+              Divider(
+                color: isDarkMode ? Colors.white60 : Colors.black45,
+              ),
+            ],
           ),
-        ),
-      );
-    }
-    return wordWidget;
+        );
   }
 }
+
