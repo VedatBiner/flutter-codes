5d8a8f4855a942223d6496a2f937a68def76f9b8
Vedat Biner <vbiner@gmail.com>
2024-06-25 16:24:02 +0300

    Sözlük App Ser-Tr - Ver 2. some fixes

--- sozluk_ser_tr_v2/lib/screens/login_page.dart
+++ sozluk_ser_tr_v2/lib/screens/login_page.dart
@@ -7,7 +7,6 @@
 library;
 
 import 'package:flutter/material.dart';
-import 'dart:developer';
 
 import '../constants/app_constants/constants.dart';
 import '../constants/app_constants/color_constants.dart';
