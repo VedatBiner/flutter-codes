3827fe4667c94309da60fc8b9325aea9cde16b04
Vedat Biner <56477545+VedatBiner@users.noreply.github.com>
2024-06-29 09:25:10 +0300

    Delete sozluk_app_ser_tr_fbfs directory

--- sozluk_app_ser_tr_fbfs/lib/constants/base_constants/splash_constant.dart
+++
@@ -1,8 +0,0 @@
-/// <----- splash_constant.dart ----->
-
-part of 'app_const.dart';
-
-class _SplashConstant {
-  final String appBarTitle = "Welcome";
-  final Duration splashDuration = const Duration(seconds: 5);
-}
