4113efd4847240795823385cf98fa096724ebb2e
Vedat Biner <vbiner@gmail.com>
2024-12-18 15:01:34 +0300

    Sözlük App v2 fixes

--- sozluk_ser_tr_v2/lib/screens/auth_page_parts/auth_common_widget.dart
+++ sozluk_ser_tr_v2/lib/screens/auth_page_parts/auth_common_widget.dart
@@ -5,7 +5,7 @@
 /// şifremi unuttum
 /// kutularını ve seçeneklerini gösteren metod
 
-library auth_page_widgets;
+library;
 
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
