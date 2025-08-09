a49f41b7665c4d6615d7f93832fd6947199b17cb
Vedat Biner <vbiner@gmail.com>
2025-08-09 17:37:03 +0300

    Malzeme List SQL App - some fixes

--- malzeme_list_sql_app/android/app/build.gradle.kts
+++ malzeme_list_sql_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.malzeme_list_sql_app"
+    namespace = "aa.vb.malzeme_list_sql_app"
     compileSdk = flutter.compileSdkVersion
    // ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.malzeme_list_sql_app"
+        applicationId = "aa.vb.malzeme_list_sql_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
