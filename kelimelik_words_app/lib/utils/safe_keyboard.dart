78a3ef271f4bc46c2129259c20064187fdeb3108
Vedat Biner <vbiner@gmail.com>
2025-12-05 13:58:26 +0300

    Kelimelik Words SQL App - some fixes

--- kelimelik_words_app/lib/utils/safe_keyboard.dart
+++ kelimelik_words_app/lib/utils/safe_keyboard.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 
 /// Güvenli şekilde HardwareKeyboard.instance çağırır.
 /// Binding hazır değilse hiçbir şey yapmaz.
 void safeClearKeyboardState() {
   try {
-    if (WidgetsBinding.instance != null) {
-      HardwareKeyboard.instance.clearState();
-    }
-  } catch (_) {
+    HardwareKeyboard.instance.clearState();
+    } catch (_) {
     // binding hazır değilse sessiz geç
   }
 }
